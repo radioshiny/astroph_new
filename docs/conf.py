d6b5188a31cde1640ea682617c15b224834cd6c7
radioshiny <shinykim@kasi.re.kr>
2023-03-16 11:51:32 +0900

    update version to publish

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'astroph_new'
 copyright = '2023, Shinyoung Kim'
 author = 'Shinyoung Kim'
-version = '0.1.8'
+version = '0.1.9'
 release = version
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="astroph_new", # Replace with your own username
-    version="0.1.8",
+    version="0.1.9",
     author="Shinyoung Kim",
     author_email="radioshiny@gmail.com",
     description="python module to make summary of astro-ph based on user interests",
